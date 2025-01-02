61ef9d43d34c447c352d495c672d09bfcdc5ed19
Amit Kulkarni <pythoncpp@users.noreply.github.com>
2025-01-02 19:57:28 +0530

    Update common.sh

--- day0/scripts/common.sh
+++ day0/scripts/common.sh
@@ -32,7 +32,7 @@ sudo apt-get install -y apt-transport-https ca-certificates curl gpg software-pr
 
 # add CRI-O key and repository
 curl -fsSL https://pkgs.k8s.io/addons:/cri-o:/stable:/$CRIO_VERSION/deb/Release.key |
-    gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
+    sudo gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://pkgs.k8s.io/addons:/cri-o:/stable:/$CRIO_VERSION/deb/ /" |
     tee /etc/apt/sources.list.d/cri-o.list
 
